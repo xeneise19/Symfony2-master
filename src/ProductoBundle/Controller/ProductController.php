d1fb86890ca4cb9282f317888b4ef5c099b5c783
xeneise19 <xeneise19@gmail.com>
2018-11-15 17:46:46 -0300

    Modo Operacion Ajax/No Ajax

--- src/ProductoBundle/Controller/ProductController.php
+++ src/ProductoBundle/Controller/ProductController.php
@@ -5,6 +5,7 @@ namespace ProductoBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
@@ -18,9 +19,12 @@ class ProductController extends Controller
 		    	->getRepository('ProductoBundle:Producto')
 		    	->find($id);
 
-        return $this->render('ProductoBundle:Default:view.html.twig' , [
-    																		'producto'=> $producto
-    																		]);
+        return $this->render(
+            'ProductoBundle:Default:view.html.twig' ,
+            [
+                'producto'    => $producto,
+                'cart_config' => $this->container->getParameter('cart_config')
+            ]);
     }
     /**
      *@Route("/products/add" , name="product_add_cart", methods="POST")
@@ -28,16 +32,29 @@ class ProductController extends Controller
     public function addToCartAction(Request $r){
         $id=$r->get('id');
         $quantity=$r->get('quantity');
+
+        $requestType=strtolower($r->headers->get('X-Request-With'));
+        $isAjax='xmlhttprequest' === $requestType;
+
         $producto= $this->getDoctrine()
         ->getRepository('ProductoBundle:Producto')
         ->find($id);
         if(null===$producto){
             throw new \Exception("Product not found");
         }
-
         $cartService = $this->get('app.cart');
         $cartService->add($producto);
-        die();   
+        if(true===$isAjax){
+            $response= new Response();
+            $response->headers->add([
+                                        'Content-Type'=>'application/json'
+                                    ]);
+            $response->setContent(json_encode($cartService->getAll()));
+            return $response;
+        }
+        return $this->redirect(
+            $this->generateURL('product_view_cart')
+            );   
     }
     /**
      *@Route("/products/cart/view" , name="product_view_cart")

6a3f71984d61d6924276d4fa0f7979137290fb49
xeneise19 <xeneise19@gmail.com>
2018-11-21 11:08:39 -0300

    Se subio FOS UserBundle nuevamente por no haberse commiteado

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new ProductoBundle\ProductoBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
